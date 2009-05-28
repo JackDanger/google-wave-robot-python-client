5b4088f3ea030185311415e4403a3cf4ed790a11
guitardave <guitardave@2100b2f4-4a30-11de-9ab4-498524d4d007>
2009-05-28 06:01:34 +0000

    updated tutorial
    git-svn-id: http://wave-robot-python-client.googlecode.com/svn/trunk@8 2100b2f4-4a30-11de-9ab4-498524d4d007

--- samples/dummy/dummy.py
+++ samples/dummy/dummy.py
@@ -1,48 +1,31 @@
-#!/usr/bin/python2.4
-#
-# Copyright 2009 Google Inc. All Rights Reserved.
-"""Implementation of Dummy robot."""
+"""Dummy robot only."""
 
 __author__ = 'davidbyttow@google.com (David Byttow)'
 
-
-import logging
-
-from api import events
-from api import model
-from api import robot
-
-# Globals
-ROBOT_NAME = 'dummy'
-LOCAL_NAME = 'davidb'
-
-
-def IsRobotInList(l):
-  """Determines if this robot is in the participant list."""
-  for participant in l:
-    if participant.startswith(ROBOT_NAME) or participant.startswith(LOCAL_NAME):
-      return True
-  return False
+from waveapi import events
+from waveapi import model
+from waveapi import robot
 
 
 def OnParticipantsChanged(properties, context):
-  """Invoked when any participants have been added/removed from the wavelet."""
+  """Invoked when any participants have been added/removed."""
   added = properties['participantsAdded']
-  if IsRobotInList(added):
-    Setup(context)
+  for p in added:
+    if p == 'dummy-tutorial@appspot.com':
+      Setup(context)
+      break
 
 
 def Setup(context):
   """Called when this robot is first added to the wave."""
   root_wavelet = context.GetRootWavelet()
-  root_wavelet.CreateBlip().GetDocument().SetText("Hello there!")
-  blip = context.GetBlipById(root_wavelet.GetRootBlipId())
-  blip.GetDocument().AppendInlineBlip()
+  root_wavelet.CreateBlip().GetDocument().SetText("I'm alive!")
+
 
 if __name__ == '__main__':
-  dummy = robot.Robot(ROBOT_NAME.capitalize(),
-                      image_url='/public/%s.png' % ROBOT_NAME,
-                      profile_url='_wave/profile.xml')
+  dummy = robot.Robot('Dummy',
+                      image_url='http://dummy.appspot.com/icon.png',
+                      profile_url='http://dummy.appspot.com/')
   dummy.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED,
                         OnParticipantsChanged)
-  dummy.Run(debug=True)
+  dummy.Run()
