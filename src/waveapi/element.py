bd3e22be97db064d1da53a5855e917ca08aeb965
pamela.fox <pamela.fox@2100b2f4-4a30-11de-9ab4-498524d4d007>
2010-04-01 11:56:43 +0000

    Adding new Wave style elements
    git-svn-id: http://wave-robot-python-client.googlecode.com/svn/trunk@26 2100b2f4-4a30-11de-9ab4-498524d4d007

--- src/waveapi/element.py
+++ src/waveapi/element.py
@@ -24,7 +24,7 @@ import logging
 import sys
 
 import util
-
+import random
 
 class Element(object):
   """Elements are non-text content within a document.
@@ -274,11 +274,23 @@ class Gadget(Element):
   def serialize(self):
     """Gadgets allow for None values."""
     return {'properties': self._properties, 'type': self._type}
-  
+
   def keys(self):
     """Get the valid keys for this gadget."""
     return [x for x in self._properties.keys() if x != 'url']
 
+class Marquee(Gadget):
+
+  def __init__(self, text=''):
+    super(Marquee, self).__init__('http://imagine-it.org/google/wave/marquee.xml', {'text': text})
+
+
+class Blink(Gadget):
+
+  def __init__(self, text=''):
+    width = len(text)*9
+    url = 'http://imagine-it.org/google/wave/blink.php?width=%s' % width
+    super(Blink, self).__init__(url, {'text': text})
 
 class Installer(Element):
   """An installer element."""
@@ -309,6 +321,18 @@ class Image(Element):
     props = dict([(key.encode('utf-8'), value)
                   for key, value in props.items()])
     return apply(Image, [], props)
+
+class LolCat(Image):
+  """An image element."""
+
+  class_type = 'IMAGE'
+
+  def __init__(self, mini=False): 
+    rand_img = random.randint(1000, 1600)
+    url = 'http://lolcat.com/images/lolcats/%s.jpg' % rand_img
+    if mini:
+      url = 'http://lolcat.com/images/lolcats/sml_%s.jpg' % rand_img
+    super(LolCat, self).__init__(url=url)
 
 
 def is_element(cls):
